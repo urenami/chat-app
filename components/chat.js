de66481b52953dc5b4ae06cf7a297a7c2b08efd5
urenami <122377715+urenami@users.noreply.github.com>
2023-08-19 01:03:41 -0400

    Updated files task 5.2

--- components/chat.js
+++ components/chat.js
@@ -1,26 +1,125 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, TextInput, TouchableOpacity, Text } from 'react-native';
+import { GiftedChat, Message } from 'react-native-gifted-chat';
 
 const Chat = ({ route, navigation }) => {
   const { name, backgroundColor } = route.params;
 
+  const [messages, setMessages] = useState([]);
+  const [inputText, setInputText] = useState('');
+  const [messageCounter, setMessageCounter] = useState(1); // Add this state for generating unique _id values
+
   useEffect(() => {
     navigation.setOptions({ title: name });
+    setMessages([
+      {
+        _id: messageCounter,
+        text: `Welcome to the chat, ${name}!`,
+        system: true,
+        createdAt: new Date(),
+      },
+      {
+        _id: messageCounter + 1,
+        text: 'Hello, this is a user message.',
+        createdAt: new Date(),
+        user: {
+          _id: 1,
+          name: name,
+        },
+      },
+    ]);
+    setMessageCounter(messageCounter + 2); // Increment counter for future messages
   }, []);
 
- return (
-   <View style={[styles.container, { backgroundColor: backgroundColor }]}>
-     <Text>Hello!</Text>
-   </View>
- );
-}
+  const onSend = (newMessages = []) => {
+    // Generate unique _id values for new messages
+    const newMessageArray = newMessages.map((message) => ({
+      ...message,
+      _id: messageCounter + newMessages.indexOf(message),
+    }));
+
+    setMessages((prevMessages) =>
+      GiftedChat.append(prevMessages, newMessageArray)
+    );
+    setInputText('');
+    setMessageCounter(messageCounter + newMessages.length); // Increment counter for future messages
+  };
+
+  return (
+    <View style={[styles.container, { backgroundColor: backgroundColor }]}>
+      <GiftedChat
+        messages={messages}
+        onSend={(newMessages) => onSend(newMessages)}
+        user={{
+          _id: 1,
+          name: name,
+        }}
+        listViewProps={{
+          style: { paddingTop: 20 },
+        }}
+        renderInputToolbar={() => (
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              multiline
+              placeholder="Type a message..."
+              value={inputText}
+              onChangeText={setInputText}
+            />
+            <TouchableOpacity
+              style={styles.sendButton}
+              onPress={() =>
+                onSend([{ text: inputText, user: { _id: 1 } }])
+              }
+            >
+              <Text style={styles.sendButtonText}>Send</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+        renderMessage={(props) => {
+          const textStyle = {
+            color: 'white',
+          };
+          return <Message {...props} textStyle={textStyle} />;
+        }}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderTopWidth: 1,
+    borderTopColor: '#ccc',
+    marginTop: -20,
+  },
+  input: {
+    flex: 1,
+    padding: 10,
+    borderRadius: 24,
+    backgroundColor: '#F2F2F2',
+    color: '#333',
+    marginRight: 10,
+  },
+  sendButton: {
+    backgroundColor: '#007AFF',
+    borderRadius: 20,
+    paddingVertical: 8,
+    paddingHorizontal: 15,
     justifyContent: 'center',
-    alignItems: 'center'
-  }
+  },
+  sendButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 });
 
 export default Chat;
--- components/start.js
+++ components/start.js
@@ -1,94 +1,133 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text, Button, TextInput, ImageBackground, TouchableOpacity } from 'react-native';
 
+
 const Start = ({ navigation }) => {
-  const [name, setName] = useState('');
-  const [color, setColor] = useState('');
+const [name, setName] = useState('');
+const [color, setColor] = useState('');
 
-  const bgColors = {
-    dark: '#090C08',
-    lightgrey: '#8A95A5',
-    darkpurple: '#474056',
-    olive: '#B9C6AE',
-  };
 
-  const { dark, lightgrey, darkpurple, olive } = bgColors;
+const bgColors = {
+dark: '#090C08',
+lightgrey: '#8A95A5',
+darkpurple: '#474056',
+olive: '#B9C6AE',
+};
 
-  return (
-    <ImageBackground
-      source={require('../assets/BackgroundImage.png')}
-      style={styles.container}
-    >
-      <Text>Hello!</Text>
-      <TextInput
-        style={styles.textInput}
-        value={name}
-        onChangeText={setName}
-        placeholder='Type your username here'
-      />
 
-      <View>
-        <Text>Choose your background colour:</Text>
-        <View>
-          <TouchableOpacity
-            style={[styles.colorSelect__dot, { backgroundColor: dark }]}
-            onPress={() => setColor(dark)}
-          />
+const { dark, lightgrey, darkpurple, olive } = bgColors;
 
-          <TouchableOpacity
-            style={[styles.colorSelect__dot, { backgroundColor: lightgrey }]}
-            onPress={() => setColor(lightgrey)}
-          />
 
-          <TouchableOpacity
-            style={[styles.colorSelect__dot, { backgroundColor: darkpurple }]}
-            onPress={() => setColor(darkpurple)}
-          />
+return (
+<ImageBackground
+source={require('../assets/BackgroundImage.png')}
+style={styles.container}
+>
+<Text>Hello!</Text>
+<TextInput
+style={styles.textInput}
+value={name}
+onChangeText={setName}
+placeholder='Type your username here'
+/>
 
-          <TouchableOpacity
-            style={[styles.colorSelect__dot, { backgroundColor: olive }]}
-            onPress={() => setColor(olive)}
-          />
-        </View>
-        <TouchableOpacity
-          style={styles.chatButton}
-          onPress={() => navigation.navigate('Chat', { name: name, backgroundColor: color })}>
-        <Text style={styles.chatButton__text}>Go to Chat</Text>
-        </TouchableOpacity>
-      </View>
-    </ImageBackground>
-  );
+
+<View>
+<Text>Choose your background colour:</Text>
+<View style={styles.colorSelect__container}>
+<TouchableOpacity
+style={[
+styles.colorSelect__dot,
+{ backgroundColor: dark },
+color === dark ? styles.selectedColor : null,
+]}
+onPress={() => setColor(dark)}
+/>
+
+
+<TouchableOpacity
+style={[
+styles.colorSelect__dot,
+{ backgroundColor: lightgrey },
+color === lightgrey ? styles.selectedColor : null,
+]}
+onPress={() => setColor(lightgrey)}
+/>
+
+
+<TouchableOpacity
+style={[
+styles.colorSelect__dot,
+{ backgroundColor: darkpurple },
+color === darkpurple ? styles.selectedColor : null,
+]}
+onPress={() => setColor(darkpurple)}
+/>
+
+
+<TouchableOpacity
+style={[
+styles.colorSelect__dot,
+{ backgroundColor: olive },
+color === olive ? styles.selectedColor : null,
+]}
+onPress={() => setColor(olive)}
+/>
+</View>
+<TouchableOpacity
+style={styles.chatButton}
+onPress={() =>
+navigation.navigate('Chat', { name: name, backgroundColor: color })
 }
+>
+<Text style={styles.chatButton__text}>Go to Chat</Text>
+</TouchableOpacity>
+</View>
+</ImageBackground>
+);
+};
+
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  textInput: {
-    width: '88%',
-    padding: 15,
-    borderWidth: 1,
-    marginTop: 15,
-    marginBottom: 15
-  },
-  colorSelect__dot: {
-    width: 30,
-    height: 30,
-    borderRadius: 15,
-    margin: 5,
-  },
-  chatButton: {
-    backgroundColor: "#757083",
-    justifyContent: "center",
-    width: "88%",
-    padding: 16,
-  },
-  chatButton__text: {
-    color: "#fff",
-    fontWeight: "600",
-  },
+container: {
+flex: 1,
+justifyContent: 'center',
+alignItems: 'center',
+},
+textInput: {
+width: '88%',
+padding: 15,
+borderWidth: 1,
+marginTop: 15,
+marginBottom: 15,
+},
+colorSelect__container: {
+flexDirection: 'row',
+alignItems: 'center',
+},
+colorSelect__dot: {
+width: 30,
+height: 30,
+borderRadius: 15,
+margin: 5,
+},
+selectedColor: {
+borderWidth: 2,
+borderColor: '#fff', // Change to white (#fff)
+},
+chatButton: {
+backgroundColor: '#757083',
+justifyContent: 'center',
+width: '88%',
+padding: 16,
+},
+chatButton__text: {
+color: '#fff',
+fontWeight: '600',
+},
 });
 
+
 export default Start;
+
+
