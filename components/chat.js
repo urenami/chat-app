8acaaf765b6ceaf978c3ed728dad1b11181b542d
urenami <122377715+urenami@users.noreply.github.com>
2023-08-30 10:15:30 -0400

    integrating features updated files

--- components/chat.js
+++ components/chat.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Alert } from "react-native";
 import { GiftedChat, InputToolbar, Message } from "react-native-gifted-chat";
 import {
   collection,
@@ -9,12 +9,65 @@ import {
   addDoc,
 } from "firebase/firestore";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import CustomActions from "./customactions";
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'; 
+import * as ImagePicker from 'expo-image-picker'; 
+import * as Location from 'expo-location';  
 
-const Chat = ({ route, navigation, database, isConnected }) => {
+const Chat = ({ route, navigation, database, storage, isConnected }) => {
   const { userId, userName, backgroundColor } = route.params;
 
   const [messages, setMessages] = useState([]);
-  const [inputText, setInputText] = useState("");
+
+  const generateReference = (uri) => {
+    const timeStamp = (new Date()).getTime();
+    const imageName = uri.split("/")[uri.split("/").length - 1];
+    return `${userId}-${timeStamp}-${imageName}`;
+  };
+
+  const uploadAndSendImage = async (imageURI) => {
+    const uniqueRefString = generateReference(imageURI);
+    const newUploadRef = ref(storage, uniqueRefString);
+    const response = await fetch(imageURI);
+    const blob = await response.blob();
+    uploadBytes(newUploadRef, blob).then(async (snapshot) => {
+      const imageURL = await getDownloadURL(snapshot.ref)
+      onSend([{ image: imageURL }]);
+    });
+  };
+
+  const pickImage = async () => {
+    let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (permissions?.granted) {
+      let result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) await uploadAndSendImage(result.uri);
+      else Alert.alert("Permissions haven't been granted.");
+    }
+  };
+
+  const takePhoto = async () => {
+    let permissions = await ImagePicker.requestCameraPermissionsAsync();
+    if (permissions?.granted) {
+      let result = await ImagePicker.launchCameraAsync();
+      if (!result.cancelled) await uploadAndSendImage(result.uri);
+      else Alert.alert("Permissions haven't been granted.");
+    }
+  };
+
+  const getLocation = async () => {
+    let permissions = await Location.requestForegroundPermissionsAsync();
+    if (permissions?.granted) {
+      const location = await Location.getCurrentPositionAsync({});
+      if (location) {
+        onSend({
+          location: {
+            longitude: location.coords.longitude,
+            latitude: location.coords.latitude,
+          },
+        });
+      } else Alert.alert("Error occurred while fetching location");
+    } else Alert.alert("Permissions haven't been granted.");
+  };
 
   useEffect(() => {
     navigation.setOptions({ title: userName });
@@ -58,17 +111,26 @@ const Chat = ({ route, navigation, database, isConnected }) => {
     }
   };
 
-  const onSend = (newMessages) => {
-    addDoc(collection(database, "messages"), {
-      text: newMessages[0].text,
-      createdAt: new Date(),
-      user: {
-        _id: userId,
-        name: userName,
-      },
-    });
+  const onSend = (newMessages = []) => {
+    if (newMessages.length > 0) {
+      addDoc(collection(database, "messages"), {
+        text: newMessages[0].text,
+        createdAt: new Date(),
+        user: {
+          _id: userId,
+          name: userName,
+        },
+      });
+    }
+  };
 
-    setInputText("");
+  const renderCustomActions = (props) => {
+    return <CustomActions
+      {...props}
+      onPickImage={pickImage}  
+      onTakePhoto={takePhoto}   
+      onGetLocation={getLocation} 
+    />;
   };
 
   return (
@@ -84,19 +146,13 @@ const Chat = ({ route, navigation, database, isConnected }) => {
           style: { paddingTop: 20 },
         }}
         renderInputToolbar={renderInputToolbar}
-        // ...
+        renderActions={renderCustomActions}
         renderMessage={(props) => {
           return (
             <Message
               {...props}
               containerStyle={styles.inputContainer}
               textInputStyle={styles.input}
-              renderInputToolbar={(props) => (
-                <InputToolbar
-                  {...props}
-                  containerStyle={styles.inputToolbarContainer}
-                />
-              )}
             />
           );
         }}
@@ -141,6 +197,5 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
 });
-
 
 export default Chat;
